7d7d85fce7de08f3deab6e6b582aab297580f3ee
Martin <martin.g.cao@gmail.com>
2023-04-20 07:40:35 +1000

    Read csv

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,36 @@
 const { chromium, selectors } = require('playwright');
 const args = require('yargs').argv;
+const fs = require('fs');
+const csv = require('csv-parser');
 
 async function main() {
   console.time('main')
 
-  console.log('Hello world!')
+  const filePath = 'inputs/data.csv';
 
-  console.log('=========')
-  console.log('END OF SCRIPT')
-  console.log('=========')
-  console.timeEnd('main')
-  process.exit()
+  if (!fs.existsSync(filePath)) {
+    console.error(`File not found: ${filePath}`);
+    process.exit(1);
+  }
+
+  // Parse the CSV data into an array of objects using the csv-parser package
+  const csvData: any[] = [];
+
+  // Use the csv-parser package to parse the CSV data
+  fs.createReadStream(filePath)
+    .pipe(csv())
+    .on('data', (data) => {
+      // Create an object for each row of data
+      const row = {};
+      for (const key in data) {
+        // Use the column names as keys and the row values as values
+        row[key] = data[key];
+      }
+      // Add the object to the csvData array
+      csvData.push(row);
+      console.log(row)
+    })
+    
 }
 
 function parseArgs() {
